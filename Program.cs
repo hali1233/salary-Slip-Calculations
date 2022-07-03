d0dfc9ba1021f73c6f21775eec79e60ee52a0f38
hali1233 <108620916+hali1233@users.noreply.github.com>
2022-07-04 00:48:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class Salary //parent
+    {
+        int ID;
+        string Name;
+        int salary;
+        string Emp_category;
+        public int gros_salary;
+        public void Setdata(int id, string nam, int sal, string cat) 
+        {
+            ID = id;
+            Name = nam;
+            salary = sal;
+            Emp_category = cat;
+        
+        }
+        public void GrossSalary(int med_allow, int fuel_allow)
+        {
+            gros_salary = salary + med_allow + fuel_allow;
+
+        }
+        public void Getdata()
+        {
+            Console.WriteLine("Employee ID: {0}",ID);
+            Console.WriteLine("Employee Name: {0}",Name);
+            Console.WriteLine("Employee Salary: {0}",salary);
+        Console.WriteLine("Employee Category: {0}",Emp_category);
+            Console.WriteLine("Gross Salary is: {0}", gros_salary);
+        }
+       
+    
+    }
+    internal class Program { 
+        static void Main(string[] args)
+        {
+            Salary s = new Salary();
+            s.Setdata(1,"Hamza",310000,"Enigneer");
+            s.GrossSalary(21000,105000);
+            s.Getdata();
+            Console.WriteLine();
+            s.Setdata(2, "Usama", 310000, "Manager");
+            s.GrossSalary(52500, 210000);
+            s.Getdata();
+            Console.WriteLine();
+            s.Setdata(3, "Ibrahim", 310000, "Analysts");
+            s.GrossSalary(31500, 168000);
+            s.Getdata();
+        }
+    }
+}
